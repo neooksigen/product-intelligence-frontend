0a88fe1f5b30ff7d1302f8343ede9719b1d2b127
neooksigen <neooksigen@windowslive.com>
2026-05-23 11:10:08 +0700

    Update raw page.tsx enhance date visual

--- src/app/raw/page.tsx
+++ src/app/raw/page.tsx
@@ -18,7 +18,7 @@ export default async function Page() {
   const transformed = safeData.map(d => ({
     ...d,
     date: d.timestamp_extract_utc
-    ? d.timestamp_extract_utc.split(" ")[0]  // ✅ take only YYYY-MM-DD
+    ? d.timestamp_extract_utc.slice(0, 10)  // ✅ take only YYYY-MM-DD edited 23 may 2026
     : "",
     count_products: d.count_products  //30 april 2026 added recommended by chatgpt (optional)  
   }))
